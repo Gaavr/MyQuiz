93cc1ac30213f3f81066665878e37e5d5580e749
Gaavr <gavrplay@gmail.com>
2021-08-08 06:45:50 +0300

    without test class client

--- src/main/java/com/gaavr/myquiz/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gaavr.myquiz.repository;
-
-import com.gaavr.myquiz.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
